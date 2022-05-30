36ab0680cb0ea76fedd2215faa30605b5576a342
Spyros Babalitis <spyros.babalitis@gmail.com>
2022-05-31 00:27:03 +0100

    Sidebar template complete

--- spotify-clone/pages/index.js
+++ spotify-clone/pages/index.js
@@ -1,18 +1,11 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Spotify</title>
-      </Head>
-
-      <h1>Spotify 2.0 Build</h1>
-
-      <main>
+    <div className="bg-black h-screen overflow-hidden">
+      <main className=''>
         <Sidebar/>
         {/* Center */}
       </main>

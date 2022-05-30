cf098fd4caf9d149b8ce02b7462cb8b7a437dab8
Spyros Babalitis <spyros.babalitis@gmail.com>
2022-05-30 19:35:21 +0100

    App start, sidebar start

---
+++ spotify-clone/pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Spotify</title>
+      </Head>
+
+      <h1>Spotify 2.0 Build</h1>
+
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}

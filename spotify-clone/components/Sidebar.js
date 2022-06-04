06236d2d5785faf9ec983ce48159734ae7514808
Spyros Babalitis <spyros.babalitis@gmail.com>
2022-06-04 21:54:22 +0100

    Spotify auth fix!

--- spotify-clone/components/Sidebar.js
+++ spotify-clone/components/Sidebar.js
@@ -9,8 +9,8 @@ import {
 import React, { signOut, useSession } from "next-auth/react";
 
 function Sidebar() {
-    //const { data: session, status } = useSession();
-    //console.log(session);
+    const { data: session, status } = useSession();
+    console.log(session);
   return (
     <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
         <div className='space-y-4'>

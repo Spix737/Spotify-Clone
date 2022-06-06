2ef648f6e51fd903ac722b6115237fa3401d950a
Spyros Babalitis <spyros.babalitis@gmail.com>
2022-06-06 22:17:00 +0100

    Middleware.js complete
    Fixed up middleware
    Redirects users that aren't signed in to login page

--- spotify-clone/pages/_middleware.js
+++ spotify-clone/pages/_middleware.js
@@ -7,5 +7,20 @@ export async function middleware(req) {
 
     const { pathname } = req.nextUrl;
 
-     
+    //Allow the requests if the following is true:
+    //1. It is a request for fetching next-auth session and provider
+    //2. The token exists
+
+    if(pathname.includes("/api/auth/") ||  token){ 
+        return NextResponse.next();
+    };
+
+    //If the token does not exist and the user requests a 
+    //protected route, redirect to login page
+    if(!token && pathname !=="/login"){
+        const url = req.nextUrl.clone()
+        url.pathname = "/login";
+        return NextResponse.redirect(url);
+    }
 }
+

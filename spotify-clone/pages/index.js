d0702e9616cc784332a6e3338a56bbb68cafba2d
Spyros Babalitis <spyros.babalitis@gmail.com>
2022-06-07 01:37:41 +0100

    Center UI Part 1
    top right header section
    from-bg-colours
    css changes
    tailwind-scrollbar-hide plugin install

--- spotify-clone/pages/index.js
+++ spotify-clone/pages/index.js
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main className=''>
+      <main className='flex'>
         <Sidebar/>
         <Center />
       </main>
